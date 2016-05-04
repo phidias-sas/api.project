4eef21107e6fd00394b78c5efd578fdae376e0d8
Santiago Cortes <santiago.cortes@phidias.com.co>
2016-05-04 18:52:17 -0500

    added public folder

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+include "../vendor/autoload.php";
+
+// Import modules:
+//Server::import("../path/to/module");
+//Server::import("../vendor/phidias/api/modules/debug.api");
+//Server::import("../vendor/phidias/documentation.api");
+//Server::import("../vendor/phidias/oauth.api");
+//Server::import("../vendor/phidias/calendar.api");
+//Server::import("../vendor/phidias/icons.api");
+
+Server::run();
